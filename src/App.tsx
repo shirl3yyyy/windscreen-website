225891d3d986cf880aeff1a5b7138a6c1248721c
shirl3yyyy <shirleycaren254@gmail.com>
2026-09-07 16:33:10 +0300

    home hero and some modifications for navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,12 @@
 import './App.css'
 import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
 
 function App() {
   return (
     <>
       <Navbar />
-     <main>
-        <h1>Welcome to MyApp</h1>
-        <p>This is a sample application built with React and Vite.</p>
-      </main>
+      <Hero />
     </>
   );
 }
---
+++ src/components/Hero.tsx
@@ -0,0 +1,21 @@
+import "./Hero.css"
+
+function Hero() {
+    return (
+        <section className="hero">
+            <div className="hero-content">
+                <h1>Get a life!</h1>
+                <p>And protect your energy.</p>
+
+                <div className="hero-vuttons">
+                    <button className="primary-button">
+                        Start Now
+                    </button>
+                </div>
+
+                </div>
+                </section>
+    );
+}
+
+export default Hero;
